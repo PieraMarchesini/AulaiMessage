ed41b0bb5a18e93a2db7c2283b1a37a3d04aacfc
Piera Marchesini <pieracarolina@yahoo.com.br>
2018-02-07 13:03:19 -0200

    First commit

---
+++ AulaiMessage/AulaiMessage MessagesExtension/MessagesViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MessagesViewController.swift
+//  AulaiMessage MessagesExtension
+//
+//  Created by Piera Marchesini on 07/02/18.
+//  Copyright © 2018 Piera Marchesini. All rights reserved.
+//
+
+import UIKit
+import Messages
+
+class MessagesViewController: MSMessagesAppViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Action
+    @IBAction func startButtonPressed(_ sender: Any) {
+    }
+
+}
